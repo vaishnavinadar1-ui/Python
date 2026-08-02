db890a61ce2f65bbcc16d1185ac6900c8590e479
Vaishnavi Nadar <nadarvaishnavi1@gmail.com>
2026-08-02 17:34:34 +0530

    Add files via upload

---
+++ coke.py
@@ -0,0 +1,13 @@
+amount_due = 50
+
+while amount_due > 0:
+    print("Amount Due:", amount_due)
+
+    coin = int(input("Insert Coin: "))
+
+    if coin == 25 or coin == 10 or coin == 5:
+        amount_due = amount_due - coin
+
+change = abs(amount_due)
+
+print("Change Owed:", change)
