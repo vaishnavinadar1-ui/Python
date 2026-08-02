5ff78cebcfde6bb6902c8cb2bc0080cd6b4e4b0a
Vaishnavi Nadar <nadarvaishnavi1@gmail.com>
2026-08-02 17:31:52 +0530

    Add files via upload

---
+++ camel.py
@@ -0,0 +1,11 @@
+camel = input("camelCase: ")
+
+print("snake_case: ", end="")
+
+for letter in camel:
+    if letter.isupper():
+        print("_" + letter.lower(), end="")
+    else:
+        print(letter, end="")
+
+print()
